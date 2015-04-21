01db51283b6080203c1fd23f551a7be975e55c02
SaifJerbi <m.jerbi.saif@gmail.com>
2015-04-21 16:33:26 +0100

    Adding Touchkit Dependency For mobile Adding CustomThemeLoader to preserve touchkit-addon loading the theme

---
+++ src/main/java/org/vaadin/tunis/blood_donation/widgetset/client/CustomThemeLoader.java
@@ -0,0 +1,11 @@
+package org.vaadin.tunis.blood_donation.widgetset.client;
+
+import com.vaadin.addon.touchkit.gwt.client.ThemeLoader;
+
+public class CustomThemeLoader extends ThemeLoader {
+	@Override
+	public void load() {
+		// custom theme loader
+	}
+
+}
